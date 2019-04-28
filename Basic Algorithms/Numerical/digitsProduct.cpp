d853d551e2e37bda0c070da9e3330b88eab56a01
Tran Minh Sang <sangtran.d14ptit@gmail.com>
2019-04-28 09:05:12 +0700

    update digitsProduct

--- Basic Algorithms/Numerical/digitsProduct.cpp
+++ Basic Algorithms/Numerical/digitsProduct.cpp
@@ -1,2 +1,16 @@
-
-
+int digitsProduct(int product) {
+    if (product < 10) {
+    	if (product == 0) return 10;
+		return product;
+	}
+        
+    string result;
+    for (int i = 9; i > 1; i--) {
+        while (product % i == 0) {
+            product /= i;
+            result = char('0' + i) + result;
+        }
+    }
+    if (product != 1) return -1;
+	return stoi(result);
+}
